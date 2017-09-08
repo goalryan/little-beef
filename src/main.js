243e50f1c949f2b5484b85bee928d83a86547bb7
goalryan <goalryan@qq.com>
2017-09-08 17:50:29 +0800

    添加样式

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import YDUI from 'vue-ydui';
+import 'vue-ydui/dist/ydui.rem.css';
+// import 'vue-ydui/dist/ydui.px.css';
+
+Vue.use(YDUI);
 
 new Vue({
   el: '#app',
